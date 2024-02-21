3da577d382d202dc40ab12e34cedffa5c6ba74af
plewis10apsu <145595593+plewis10apsu@users.noreply.github.com>
2024-02-20 21:28:21 -0600

    Main_Menu and Options_Menu Update
    I updated the layout of the Main _Menu, and the Options Menu. On the Main_Menu, I recentered the buttons and added temporary tiles for the Credits and Options buttons. In the Options_Menu, I added the background and updated the font for the Options label.

--- menus/Main_Menu/main_menu.gd
+++ menus/Main_Menu/main_menu.gd
@@ -7,9 +7,10 @@ func _on_start_button_pressed():
 	Global.change_level("res://level_0/scenes/level_0.tscn")
 
 
-func _on_options_button_pressed():
-	Global.change_level("res://menus/Options_Menu/options_menu.tscn")
-
-
-func _on_quit_button_pressed():
+func _on_exit_button_pressed():
 	get_tree().quit()
+
+
+
+func _on_help_button_pressed():
+	Global.change_level("res://menus/Options_Menu/options_menu.tscn")
