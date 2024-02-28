4f3828a82a7b4c2b29f2385e8754a95aa058ffd3
peggy <plewis10@my.apsu.edu>
2024-02-28 08:01:26 -0600

    Updated Main_Menu
    Separated the icons on the Main_Menu, and updated the Play button's signal function name from "start" to "play"

--- menus/Main_Menu/main_menu.gd
+++ menus/Main_Menu/main_menu.gd
@@ -3,7 +3,7 @@ extends Control
 func _ready():
 	Global.current_level = self
 
-func _on_start_button_pressed():
+func _on_play_button_pressed():
 	Global.change_level("res://level_0/tilemap_level_test.tscn")
 
 func _on_exit_button_pressed():
