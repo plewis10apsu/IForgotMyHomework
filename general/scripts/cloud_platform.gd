2e723bd37020ce3d9526d467a60a62d38529b740
structjim <159969768+structjim@users.noreply.github.com>
2024-04-17 22:33:15 -0500

    Fixed cloud crash
    Giving cloud.gd ActorData didn't fix the clash because the cloud in question was a cloud_platform.gd, but a cloud.gd!

--- general/scripts/cloud_platform.gd
+++ general/scripts/cloud_platform.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var actorData : ActorData
+
 const CYCLE_HEIGHT : int = 30 # Pixels
 const CYCLE_SPEED : float = 2 # Pi radians per second
 var cycle_angle : float = 0
@@ -7,6 +9,7 @@ var initial_position : Vector2
 
 func _ready():
 	initial_position = position
+	actorData = ActorData.new(1, TEAM.PLAYER, WEAPON.NONE, 0)
 
 func _process(delta):
 	# Update the angle
