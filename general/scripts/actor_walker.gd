e92df6797bcc4d074a3dea434d4afc6263e98a37
structjim <159969768+structjim@users.noreply.github.com>
2024-04-17 21:51:05 -0500

    Added baddie sprite, and started implementing new enemy types

--- general/scripts/actor_walker.gd
+++ general/scripts/actor_walker.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 @export var turns_at_ledges = true
-const WALK_SPEED = 80.0 #pixels per second
-var is_facing_right : bool = true
+@export var is_facing_right : bool = true
+const WALK_SPEED = 20.0 #pixels per second
 var actorData : ActorData
 var position_x_last_frame : float
 var is_in_floor_backup : bool = false
@@ -11,38 +11,30 @@ var gravity : float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	actorData = ActorData.new(1, TEAM.ENEMY, WEAPON.NONE, 1)
+	actorData = ActorData.new(100, TEAM.ENEMY, WEAPON.NONE, 1)
 	position_x_last_frame = 0
+	if !is_facing_right:
+		#Starts facing left, so make the sprite reflect this.
+		$AnimatedSprite2D.scale.x = abs($AnimatedSprite2D.scale.x) * (-1)
+		$AnimatedSprite2D
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if actorData.hp <= 0:	
 		#DIE
 		queue_free()
-
+		
 func _physics_process(delta):
 	#GRAVITY
 	velocity.y += gravity * delta
 	#MOVE AND TURN AROUND
 	if(is_on_floor()):
-		if(
-			position.x == position_x_last_frame
-			or
-			!$PhBaddie/LedgeSensor.get_overlapping_bodies().size()
-		):
-			#We haven't moved, or we have reached a ledge, so flip!
-			if is_facing_right:
-				#Switch to left
-				is_facing_right = false
-				print("R->L Scale before flip: " + str(scale.y))
-				$PhBaddie.scale.x = abs($PhBaddie.scale.x) * (-1)
-				print("R->L Scale after flip: " + str(scale.y))
-			else:##not right yet
-				#Switch to right
-				is_facing_right = true
-				print("L->R Scale before flip: " + str(scale.y))
-				$PhBaddie.scale.x = abs($PhBaddie.scale.x)
-				print("L->R Scale after flip: " + str(scale.y))
+		if(position.x == position_x_last_frame):
+			#We've bumped into a wall!
+			flip()
+		elif turns_at_ledges and !$AnimatedSprite2D/LedgeSensor.get_overlapping_bodies().size():
+			flip()
 		#Move
 		velocity.x = WALK_SPEED * (1 if is_facing_right else (-1))
 		position_x_last_frame = position.x
@@ -52,9 +44,19 @@ func _physics_process(delta):
 		#print(o)
 	move_and_slide()
 
+func flip():
+	#We haven't moved, or we have reached a ledge, so flip!
+	if is_facing_right:
+		#Switch to left
+		is_facing_right = false
+		$AnimatedSprite2D.scale.x = abs($AnimatedSprite2D.scale.x) * (-1)
+	else:##not right yet
+		#Switch to right
+		is_facing_right = true
+		$AnimatedSprite2D.scale.x = abs($AnimatedSprite2D.scale.x)
+
 func _on_area_2d_area_entered(area):
 	var other = area.get_parent()
-	print("WALKER OVERLAPPED AREA: " + str(other))
 	var is_friendly = (actorData.team == other.actorData.team)
 	var is_hazardous_actor
 	if(other.actorData != null) and other.actorData.hazard_level>0:
