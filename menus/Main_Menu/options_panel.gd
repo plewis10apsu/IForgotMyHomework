a28c20f7cdb11c10b57566890284b2b45b4d0517
Jacob <thinkinghawk1@gmail.com>
2024-04-22 12:59:03 -0500

    Implemented sound effects mute
    Implemented sound effects mute which can be toggled on the options card.

---
+++ menus/Main_Menu/options_panel.gd
@@ -0,0 +1,19 @@
+extends Panel
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$MarginContainer/VBoxContainer/VBoxContainer/SoundFXCheckButton.button_pressed = !Global.sfx_muted
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_sound_fx_check_button_toggled(toggled_on):
+	if toggled_on:
+		Global.unmute_sfx()
+		print("unmuted sound effects")
+	else:
+		Global.mute_sfx()
+		print("muted sound effects")
