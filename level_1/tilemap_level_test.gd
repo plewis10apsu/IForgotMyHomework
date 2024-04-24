40861c599e543656cb585cca842436d993018f8e
structjim <159969768+structjim@users.noreply.github.com>
2024-04-24 03:53:26 -0500

    Turned music in level 1 down

--- level_1/tilemap_level_test.gd
+++ level_1/tilemap_level_test.gd
@@ -3,4 +3,4 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Global.play_music_by_name_plus_volume("gameboy_girl", -6)
+	Global.play_music_by_name_plus_volume("gameboy_girl", -12)
