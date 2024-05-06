8a0311be3b9846dab078bfa70a2617385ce6f34a
plewis10apsu <plewis10@my.apsu.edu>
2024-05-06 18:05:20 -0500

    Added XBox Input and Font Credit
    I added XBox input for 'A' button, and gave credit to the creator of the font used.

--- menus/credits/credits.gd
+++ menus/credits/credits.gd
@@ -11,7 +11,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	credits_text.position.y -= 100 * delta
-	if Input.is_action_just_pressed("escape"):
+	if Input.is_action_just_pressed("escape") or Input.is_action_just_pressed("jump"):
 		Global.change_level("res://menus/Main_Menu/main_menu.tscn")
 	
 
